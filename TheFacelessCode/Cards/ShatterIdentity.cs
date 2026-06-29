ff08e4f9a00cb8ef9c968052eddd70f7c6ca68b6
GibbyGarbo <grassmaster607@gmail.com>
2026-06-28 22:20:02 -0500

    Shatter Identity

---
+++ TheFacelessCode/Cards/ShatterIdentity.cs
@@ -0,0 +1,31 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheFaceless.TheFacelessCode.Powers;
+
+namespace TheFaceless.TheFacelessCode.Cards;
+
+
+public class ShatterIdentity() : TheFacelessCard(2,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+    new PowerVar<ShatterIdentityPower>(2)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ShatterIdentity card = this;
+        await PowerCmd.Apply<OneWithNonsensePower>(choiceContext, card.Owner.Creature, DynamicVars.Power<ShatterIdentityPower>().BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+    AddKeyword(CardKeyword.Innate);
+    }
+}
