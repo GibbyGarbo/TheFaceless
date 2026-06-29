6e8051af98ed80fbef193cde27fd992ad1612085
GibbyGarbo <grassmaster607@gmail.com>
2026-06-28 22:58:57 -0500

    Added Psychogenesis

---
+++ TheFacelessCode/Cards/Psychogenesis.cs
@@ -0,0 +1,34 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheFaceless.TheFacelessCode.Powers;
+
+namespace TheFaceless.TheFacelessCode.Cards;
+
+
+public class Psychogenesis() : TheFacelessCard(1,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+    new PowerVar<PsychogenesisPower>(5)
+    ];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<Paranoia>()];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        Psychogenesis card = this;
+        await PowerCmd.Apply<PsychogenesisPower>(choiceContext, card.Owner.Creature, DynamicVars.Power<PsychogenesisPower>().BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+    DynamicVars.Power<PsychogenesisPower>().UpgradeValueBy(2);
+    }
+}
