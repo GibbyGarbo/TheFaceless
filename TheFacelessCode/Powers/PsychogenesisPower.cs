d6b249c7331ab8b99cb784c20f7a636ae53d0534
GibbyGarbo <grassmaster607@gmail.com>
2026-06-28 22:52:13 -0500

    New power

---
+++ TheFacelessCode/Powers/PsychogenesisPower.cs
@@ -0,0 +1,34 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheFaceless.TheFacelessCode.Powers;
+
+namespace TheFaceless.TheFacelessCode.Powers;
+
+
+public class PsychogenesisPower() : TheFacelessPower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    public override async Task AfterPowerAmountChanged(
+        PlayerChoiceContext choiceContext,
+        PowerModel power,
+        Decimal amount,
+        Creature? applier,
+        CardModel? cardSource)
+    {
+        PsychogenesisPower psychogenesisPower = this;
+        if (amount == 0M || power is Paranoia || !power.Owner.IsEnemy || applier != psychogenesisPower.Owner)
+            return;
+        psychogenesisPower.Flash();
+        await CreatureCmd.Damage(choiceContext, power.Owner, psychogenesisPower.Amount, ValueProp.Unpowered, psychogenesisPower.Owner, null);
+    }
+}
