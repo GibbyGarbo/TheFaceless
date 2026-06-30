f59c9abf615745a785ba12578b0933e3461e8579
GibbyGarbo <grassmaster607@gmail.com>
2026-06-30 13:08:35 -0500

    PGenesis works now

--- TheFacelessCode/Powers/PsychogenesisPower.cs
+++ TheFacelessCode/Powers/PsychogenesisPower.cs
@@ -3,14 +3,12 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using TheFaceless.TheFacelessCode.Powers;
 
 namespace TheFaceless.TheFacelessCode.Powers;
 
 
-public class PsychogenesisPower() : TheFacelessPower
+public class PsychogenesisPower : TheFacelessPower
 {
     public override PowerType Type =>
         PowerType.Buff;
@@ -26,7 +24,8 @@ public class PsychogenesisPower() : TheFacelessPower
         CardModel? cardSource)
     {
         PsychogenesisPower psychogenesisPower = this;
-        if (amount == 0M || power is Paranoia || !power.Owner.IsEnemy || applier != psychogenesisPower.Owner)
+        if (amount == 0M || power is not Paranoia || !power.Owner.IsEnemy || applier != psychogenesisPower.Owner || power is ITemporaryPower)
+            //(amount <= 0M && power is Paranoia && power.Owner.IsEnemy && applier == psychogenesisPower.Owner)
             return;
         psychogenesisPower.Flash();
         await CreatureCmd.Damage(choiceContext, power.Owner, psychogenesisPower.Amount, ValueProp.Unpowered, psychogenesisPower.Owner, null);
