27884386b10b743c05fc19ab9d378f1629585b5c
GibbyGarbo <grassmaster607@gmail.com>
2026-07-01 13:07:17 -0500

    Added Psyche Pool

---
+++ TheFacelessCode/Cards/PsychePool.cs
@@ -0,0 +1,30 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars; 
+using TheFaceless.TheFacelessCode.Powers;
+namespace TheFaceless.TheFacelessCode.Cards;
+
+ 
+public class PsychePool() : TheFacelessCard(2,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+    new PowerVar<PsychePoolPower>(1)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        PsychePool card = this;
+        await PowerCmd.Apply<PsychePoolPower>(choiceContext, card.Owner.Creature, DynamicVars.Power<PsychePoolPower>().BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+    AddKeyword(CardKeyword.Retain);
+    }
+}
