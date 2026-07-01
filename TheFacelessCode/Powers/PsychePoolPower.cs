16f6c39f4d9b299a605a3c2be43c55135929dfe7
GibbyGarbo <grassmaster607@gmail.com>
2026-07-01 13:08:17 -0500

    Added Psyche Pool Power

---
+++ TheFacelessCode/Powers/PsychePoolPower.cs
@@ -0,0 +1,33 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheFaceless.TheFacelessCode.Powers;
+
+public class PsychePoolPower : TheFacelessPower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+
+ 
+    
+  public override async Task AfterSideTurnStart(
+      CombatSide side,
+      IReadOnlyList<Creature> participants,
+      ICombatState combatState)
+    {
+        PsychePoolPower psychePoolPower = this;
+        int BlockPerTurn = PileType.Exhaust.GetPile(Owner.Player).Cards.Count;
+        await CreatureCmd.GainBlock(psychePoolPower.Owner, BlockPerTurn, ValueProp.Unpowered,  null);
+    }
+}
